ac8e41645981e3d9c89e2ca8984b00a093d8c616
Timo Prüße <timo@no.studio>
2021-10-29 19:51:29 +0200

    WIP

---
+++ src/CurtainPole.ts
@@ -0,0 +1,51 @@
+import { Curtain } from './Curtain';
+
+export interface CurtainPoleOptions {
+	scrollContainer?: Document | HTMLElement;
+}
+
+export class CurtainPole {
+	private curtains: Curtain[] = [];
+	private scrollContainer: Document | HTMLElement;
+	private observer: IntersectionObserver;
+	private scrollHandler: (() => void) | null = null;
+
+	constructor(options: CurtainPoleOptions = {}) {
+		const { scrollContainer = document } = options;
+
+		this.scrollContainer = scrollContainer;
+		this.observer = new IntersectionObserver(this.checkEntries);
+	}
+
+	private checkEntries = (entries: IntersectionObserverEntry[]) => {
+		const entry = entries[0];
+
+		if (entry.isIntersecting) {
+			this.scrollHandler = this.handleScroll(entry);
+			this.scrollContainer.addEventListener('scroll', this.scrollHandler);
+		} else if (this.scrollHandler) {
+			this.scrollContainer.removeEventListener('scroll', this.scrollHandler);
+			this.scrollHandler = null;
+		}
+	};
+
+	private handleScroll = (entry: IntersectionObserverEntry) => () => {
+		const curtain = this.curtains.find((c) => c.node === entry.target);
+		if (!curtain) {
+			return;
+		}
+
+		curtain.update();
+	};
+
+	hang = (node: HTMLElement) => {
+		const curtain = this.curtains.find((c) => c.node === node);
+		if (curtain) {
+			console.warn('Curtain already hanging...');
+			return;
+		}
+
+		this.curtains.push(Curtain.hang(node));
+		this.observer.observe(node);
+	};
+}
